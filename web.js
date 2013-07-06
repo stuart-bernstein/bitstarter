49e02e248da0e87ac8360ec695b691dc715aa21a
stuart-bernstein <stuart.bernstein@gmail.com>
2013-07-06 17:39:01 -0400

    Updated web.js to read file

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express'), fs=require("fs");
 
 var app = express.createServer(express.logger());
-var content=fs.readFileSync("index.html");
+var content=fs.readFileSync("index.html");  //returns a buffer because no encoding
+//var buf = new Buffer(content);
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(content));
+  response.send(content.toString());
 });
 
 var port = process.env.PORT || 5000;
